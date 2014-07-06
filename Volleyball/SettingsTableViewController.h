845568ea51082db79a906d269463caade587fb32
Frank Bara <fbara1@yahoo.com>
2014-07-06 15:31:32 -0500

    Score background colors can be set and saved in settings.

--- Volleyball/SettingsTableViewController.h
+++ Volleyball/SettingsTableViewController.h
@@ -10,16 +10,19 @@
 
 @interface SettingsTableViewController : UITableViewController <UITableViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *editSettingsButton;
 @property (weak, nonatomic) IBOutlet UIButton *homeTeamColor;
 @property (weak, nonatomic) IBOutlet UIButton *visitingTeamColor;
 @property (weak, nonatomic) IBOutlet UITextField *notificationName;
 @property (weak, nonatomic) IBOutlet UISwitch *notificationSwitch;
 @property (weak, nonatomic) IBOutlet UITextField *nameOfPlayer;
-@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *notificationLabels;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *settingsLabels;
 @property (strong, nonatomic) IBOutletCollection(UITextField) NSArray *notificationTextEntries;
+@property (weak, nonatomic) IBOutletCollection(UIButton) NSArray *settingsButtons;
 @property (weak, nonatomic) IBOutlet UILabel *settingsSavedNotification;
 
 - (IBAction)homeTeamBackgroundColor:(id)sender;
 - (IBAction)visitingTeamBackgroundColor:(id)sender;
+- (IBAction)saveSettings:(id)sender;
 
 @end
--- Volleyball/SettingsTableViewController.m
+++ Volleyball/SettingsTableViewController.m
@@ -10,30 +10,28 @@
 
 @interface SettingsTableViewController ()
 
+
 @end
 
 @implementation SettingsTableViewController
 {
-    UIBarButtonItem *saveButton;
+    UIBarButtonItem *editSettingsButton;
+    BOOL editingMode;
+    BOOL changesMade;
     
 }
+
+#pragma mark - View Methods
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
-    //Create bar button items and add them to the navigation bar
-    saveButton = [[UIBarButtonItem alloc]
-                  initWithTitle:@"Save"
+    editSettingsButton = [[UIBarButtonItem alloc]
+                  initWithTitle:@"Edit"
                   style:UIBarButtonItemStyleBordered
                   target:self
-                  action:@selector(saveSettings)];
+                  action:@selector(saveSettings:)];
     
     UIImage *image = [UIImage imageNamed:@"info.png"];
     
@@ -48,15 +46,134 @@
                                    target:self
                                    action:nil];
     fixedSpace.width = 20.0f;
-    NSArray *barButtonItems = @[saveButton, fixedSpace, infoButton];
+    NSArray *barButtonItems = @[editSettingsButton, fixedSpace, infoButton];
     self.navigationItem.rightBarButtonItems = barButtonItems;
     
-    //Hide the Save button for now because nothing needs to be saved
-    self.navigationItem.rightBarButtonItem = nil;
+    //When the screen loads, we're not in editing mode nor have changes been made
+    editingMode = NO;
+    changesMade = NO;
+    
+    //Load the saved settings
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    if ([[defaults stringForKey:@"enableNotifications"] isEqualToString:@"On"]) {
+        //User wants notifications so set switch to ON and enable all text & label fields
+        [self.notificationSwitch setOn:YES];
+        for (UILabel *label in self.settingsLabels) {
+            label.enabled = YES;
+        }
+        for (UITextField *text in self.notificationTextEntries) {
+            text.enabled = YES;
+        }
+    } else {
+        //User doesn't want notifications so set switch to OFF and disable notification fields
+        [self.notificationSwitch setOn:NO];
+        for (UITextField *text in self.notificationTextEntries) {
+            text.enabled = NO;
+        }
+    }
 }
 
-- (void)saveSettings
+- (void)viewWillAppear:(BOOL)animated
 {
+    [super viewWillAppear:animated];
+    
+    self.homeTeamColor.backgroundColor = [self getSavedScoreColors:@"homeTeamColor"];
+    self.visitingTeamColor.backgroundColor = [self getSavedScoreColors:@"visitorTeamColor"];
+}
+
+/*!
+ *  Gets the background score color and returns it
+ *
+ *  @param team Either the home or visitor team.  If home, use "homeTeamColor".
+ *  if visitor, use "visitorTeamColor".
+ *
+ *  @return UIColor that is the background score color
+ */
+- (UIColor *)getSavedScoreColors:(NSString *)team
+{
+    UIColor *teamColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
+    
+    NSData *teamData = [[NSUserDefaults standardUserDefaults] dataForKey:team];
+    
+    if (teamData != nil) {
+        teamColor = (UIColor *)[NSKeyedUnarchiver unarchiveObjectWithData:teamData];
+    } else {
+        teamColor = [UIColor blueColor];
+    }
+    
+    return teamColor;
+}
+
+
+-(UIStatusBarStyle)preferredStatusBarStyle
+{
+    //Set the UINavigation color
+    return UIStatusBarStyleLightContent;
+}
+
+#pragma mark - Save Settings
+
+/*!
+ *  Save the settings
+ *
+ *  @param sender The param is not used
+ */
+- (IBAction)saveSettings:(id)sender
+{
+    if (!editingMode) {
+        //Indicate we're now in editing mode
+        editingMode = YES;
+        
+        //Button starts out as 'Edit' and should be changed to 'Done'
+        editSettingsButton.title = @"Done";
+
+        //Enable UI elements
+        self.homeTeamColor.enabled = TRUE;
+        self.visitingTeamColor.enabled = TRUE;
+        
+        
+        //Hide left 'back' button so they can't back out without hitting
+        //'done' button first
+        self.navigationItem.leftBarButtonItem.enabled = nil;
+        
+        
+    } else {
+        //No longer in editing mode
+        editingMode = NO;
+        
+        //Put button label back to 'Edit'
+        editSettingsButton.title = @"Edit";
+        
+        //Disable UI elements
+        self.homeTeamColor.enabled = FALSE;
+        self.visitingTeamColor.enabled = FALSE;
+        
+        //Save the colors regardless if they were changed or not
+        [self saveScoreColors];
+        
+        //Show the 'back' button again so they can leave this screen
+        self.navigationItem.leftBarButtonItem.enabled = YES;
+
+    }
+
+}
+
+- (void)saveScoreColors
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    //Set the home team color
+    UIColor *colorHome = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
+    colorHome = self.homeTeamColor.backgroundColor;
+    NSData *colorHomeData = [NSKeyedArchiver archivedDataWithRootObject:colorHome];
+    [defaults setObject:colorHomeData forKey:@"homeTeamColor"];
+    
+    //Set the visiting team color
+    UIColor *colorVisitor = [UIColor colorWithRed:0 green:0 blue:0 alpha:1];
+    colorVisitor = self.visitingTeamColor.backgroundColor;
+    NSData *colorVisitorData = [NSKeyedArchiver archivedDataWithRootObject:colorVisitor];
+    [defaults setObject:colorVisitorData forKey:@"visitorTeamColor"];
     
 }
 
@@ -66,11 +183,6 @@
     [self performSegueWithIdentifier:@"supportView" sender:self];
 }
 
--(UIStatusBarStyle)preferredStatusBarStyle
-{
-    //Set the UINavigation color
-    return UIStatusBarStyleLightContent;
-}
 
 #pragma mark - Score Background Color Methods
 
@@ -85,9 +197,6 @@
     homeButtonColor = [self getRandomColor];
     self.homeTeamColor.backgroundColor = homeButtonColor;
     
-    //Enable Save button
-    self.navigationItem.rightBarButtonItem = saveButton;
-    
 }
 
 /*!
@@ -100,10 +209,7 @@
     UIColor *visitingButtonColor;
     visitingButtonColor = [self getRandomColor];
     self.visitingTeamColor.backgroundColor = visitingButtonColor;
-    
-    //Enable Save button
-    self.navigationItem.rightBarButtonItem = saveButton;
-    
+  
 }
 
 - (UIColor *)getRandomColor
@@ -188,21 +294,28 @@
 }
 */
 
-/*
-#pragma mark - Table view delegate
 
-// In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here, for example:
-    // Create the next view controller.
-    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:<#@"Nib name"#> bundle:nil];
-    
-    // Pass the selected object to the new view controller.
-    
-    // Push the view controller.
-    [self.navigationController pushViewController:detailViewController animated:YES];
-}
-*/
+//#pragma mark - Table view delegate
+//
+//// In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
+//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+////    // Navigation logic may go here, for example:
+////    // Create the next view controller.
+////    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:<#@"Nib name"#> bundle:nil];
+////    
+////    // Pass the selected object to the new view controller.
+////    
+////    // Push the view controller.
+////    [self.navigationController pushViewController:detailViewController animated:YES];
+//    
+//    //Change the selected row color so the entire row doesn't become gray when it's touched
+//    if (!editingMode) {
+//        [[tableView cellForRowAtIndexPath:indexPath] setSelectionStyle:UITableViewCellSelectionStyleNone];
+//
+//    }
+//
+//}
+
 
 @end
