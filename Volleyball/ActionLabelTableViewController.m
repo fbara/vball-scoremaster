dc5082ffe03bda09cf3d144bfbfdcc8fd6d068fa
Frank S. Bara <fbara1@yahoo.com>
2015-10-27 12:22:09 -0500

    Add ability to move ActionNames rows with a long press gesture.

--- Volleyball/ActionLabelTableViewController.m
+++ Volleyball/ActionLabelTableViewController.m
@@ -18,6 +18,7 @@
 	NSMutableArray *actionNamesList;
 	BOOL firstTimeShown;
 	NSUserDefaults *defaults;
+	NSIndexPath *m_currentIndexPath;
 }
 
 @end
@@ -50,6 +51,11 @@
 	actionNamesList = [[NSMutableArray alloc] init];
 	defaults = [NSUserDefaults standardUserDefaults];
 	actionNamesList = [defaults objectForKey:@"ActionNames"];
+	
+	//Add long press gesture for moving rows
+	UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self
+																							action:@selector(longPressGesture:)];
+	[self.tableView addGestureRecognizer:longPress];
 	
 //NEW
 
@@ -238,14 +244,121 @@
     return sectionHeader;
 }
 
-- (CGFloat)tableView:(UITableView*)tableView
-    heightForHeaderInSection:(NSInteger)section
+- (CGFloat)tableView:(UITableView*)tableView heightForHeaderInSection:(NSInteger)section
 {
     return 40;
 }
 
--(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+	return UITableViewCellEditingStyleDelete;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+	if (editingStyle == UITableViewCellEditingStyleDelete) {
+		[actionNamesList removeObjectAtIndex:indexPath.row];
+		[tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+	}
+}
+
+#pragma mark - Long Press Gesture
+-(IBAction)longPressGesture:(id)sender {
+	UILongPressGestureRecognizer *longPress = (UILongPressGestureRecognizer *)sender;
+	UIGestureRecognizerState state = longPress.state;
 	
+	CGPoint location = [longPress locationInView:self.tableView];
+	NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
+	
+	static UIView *snapshot = nil; //snapshot of the row the user is moving
+	NSIndexPath *sourceIndexPath = nil; //initial indexPath, where gesture begins
+	
+	switch (state) {
+		case UIGestureRecognizerStateBegan: {
+			if (indexPath) {
+				sourceIndexPath = indexPath;
+				
+				UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+				
+				//Take a snapshot of the selected row
+				snapshot = [self customSnapshotFromView:cell];
+				
+				//Add the snapshot as a subview, centered at cell's center
+				__block CGPoint center = cell.center;
+				snapshot.center = center;
+				snapshot.alpha = 0.0;
+				[self.tableView addSubview:snapshot];
+				[UIView animateWithDuration:0.25 animations:^{
+					//Offset for gesture location
+					center.y = location.y;
+					snapshot.center = center;
+					snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05);
+					snapshot.alpha = 0.98;
+					cell.alpha = 0.0;
+					
+				} completion:^(BOOL finished){
+					cell.hidden = YES;
+				}];
+			}
+			break;
+		}
+			
+		case UIGestureRecognizerStateChanged: {
+			CGPoint center = snapshot.center;
+			center.y = location.y;
+			snapshot.center = center;
+			
+			//Is destination valid and is it different from source?
+			if (indexPath && ![indexPath isEqual:sourceIndexPath]) {
+				//Update data source
+				[actionNamesList exchangeObjectAtIndex:indexPath.row withObjectAtIndex:sourceIndexPath.row];
+				//Move the rows
+				[self.tableView moveRowAtIndexPath:sourceIndexPath toIndexPath:indexPath];
+				//Update the source so it's in sync with the UI
+				sourceIndexPath = indexPath;
+			}
+			break;
+		}
+		default: {
+			//Clean up
+			UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:sourceIndexPath];
+			cell.hidden = NO;
+			cell.alpha = 0.0;
+			
+			[UIView animateWithDuration:0.25 animations:^{
+				snapshot.center = cell.center;
+				snapshot.transform = CGAffineTransformIdentity;
+				snapshot.alpha = 0.0;
+				cell.alpha = 1.0;
+				
+			} completion:^(BOOL finished) {
+				//sourceIndexPath = nil;
+				[snapshot removeFromSuperview];
+				snapshot = nil;
+			}];
+			break;
+		}
+	}
+}
+
+#pragma mark - Gesture Recognizer Helper
+
+//@brief returns a customized snapshot of a given view
+-(UIView *)customSnapshotFromView:(UIView *)inputView {
+	
+	//Make an image from the input view
+	UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, NO, 0);
+	[inputView.layer renderInContext:UIGraphicsGetCurrentContext()];
+	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	
+	//Create an image view
+	UIView *snapshot = [[UIImageView alloc] initWithImage:image];
+	snapshot.layer.masksToBounds = NO;
+	snapshot.layer.cornerRadius = 0.0;
+	snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
+	snapshot.layer.shadowRadius = 5.0;
+	snapshot.layer.shadowOpacity = 0.4;
+	
+	return snapshot;
 	
 }
 
@@ -263,6 +376,25 @@
 			[self deleteActionNameRow];
 	  default:
 			break;
+	}
+}
+
+-(void)slideTableViewCell:(SESlideTableViewCell *)cell willSlideToState:(SESlideTableViewCellSlideState)slideState {
+	NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+	if (indexPath) {
+		if (slideState != SESlideTableViewCellSlideStateCenter) {
+			if (m_currentIndexPath) {
+				SESlideTableViewCell *cell = [self.tableView cellForRowAtIndexPath:m_currentIndexPath];
+				[cell setSlideState:SESlideTableViewCellSlideStateCenter animated:YES];
+			}
+		}
+	}
+}
+
+-(void)slideTableViewCell:(SESlideTableViewCell *)cell didSlideToState:(SESlideTableViewCellSlideState)slideState {
+	NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+	if (slideState != SESlideTableViewCellSlideStateCenter) {
+		m_currentIndexPath = indexPath;
 	}
 }
 
